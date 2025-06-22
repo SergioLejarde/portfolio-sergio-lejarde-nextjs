7668e965227bba56da44a64aa936c2a050dc716f
SergioLejarde <80810089+SergioLejarde@users.noreply.github.com>
2025-06-22 02:41:08 -0500

    commit, aligerando y quitando cosas innecesarias que ralentizan el ddeploy

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    formats: ['image/avif', 'image/webp'],
+    dangerouslyAllowSVG: false,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
